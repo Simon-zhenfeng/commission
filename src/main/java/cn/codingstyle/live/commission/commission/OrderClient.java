066874f06748a5cf31c7545cfb6fca933671f482
simon <hanzhenfeng@wuxicloud.com>
2021-05-16 22:18:47 +0800

    修改签名，并调通测试

--- src/main/java/cn/codingstyle/live/commission/commission/OrderClient.java
+++ src/main/java/cn/codingstyle/live/commission/commission/OrderClient.java
@@ -12,6 +12,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.String.format;
+
 /**
  * @author Simon
  * @date 2021/5/15 21:42
@@ -19,7 +21,7 @@ import java.util.Map;
 public class OrderClient {
     private static final String ORDER_API_URL = "https://openapi-fxg.jinritemai.com/order/searchList?" +
             "app_key={app_key}&method={method}&access_token={access_token}&timestamp={timestamp}&v={v}" +
-            "param_json={param_json}&sign={sign}";
+            "&param_json={param_json}&sign={sign}";
 
     public List<Order> queryOrder(LocalDateTime startTime, LocalDateTime endTime) {
         HashMap<String, Object> params = new HashMap<>();
@@ -30,26 +32,56 @@ public class OrderClient {
         Map<String, String> requestBody = getParam(params, "order.searchList");
 
         RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.getForObject(ORDER_API_URL, String.class, requestBody);
-        System.out.println("response = " + response);
+        String responseBody = restTemplate.getForObject(ORDER_API_URL, String.class, requestBody);
+        System.out.println("responseBody = " + responseBody);
+        JSONObject responseData = parseResponse(responseBody);
         List<Order> orders = new ArrayList<>();
+        orders = getOrders(responseData);
         return orders;
     }
 
+    private List<Order> getOrders(JSONObject responseData) {
+        final List<Order> orders = new ArrayList<>();
+        responseData.getJSONArray("shop_order_list")
+                .forEach(item ->{
+                    JSONObject orderJson = (JSONObject) item;
+                    orders.add(Order.whole(
+                            orderJson.getString("order_id"),
+                            orderJson.getIntValue("order_status"),
+                            orderJson.getIntValue("pay_amount"),
+                            orderJson.getIntValue("create_time")
+                    ));
+                });
+        return orders;
+    }
+
+    private JSONObject parseResponse(String responseBody) {
+        JSONObject jsonObject = JSONObject.parseObject(responseBody);
+        int err_no = jsonObject.getIntValue("err_no");
+        if (err_no != 0) {
+            throw new RuntimeException(format(
+                    "Call Order API Error: Error No: %d, Error Msg: %s",
+                    err_no,
+                    jsonObject.getString("message")
+            ));
+        }
+        return jsonObject.getJSONObject("data");
+    }
+
     private Map<String, String> getParam(HashMap<String, Object> param, String method) {
-        Map<String, String> result = getCommonParam();
-        result.put("method", method);
+        Map<String, String> result = getCommonParam(method);
         result.put("param_json", JSONObject.toJSONString(param));
-        result.put("sign", JRTMAPISignUtil.getSign(getCommonParam(), param));
+        result.put("sign", JRTMAPISignUtil.getSign(getCommonParam(method), param));
         return result;
     }
 
-    private Map<String, String> getCommonParam() {
+    private Map<String, String> getCommonParam(String method) {
         Map<String, String> result = new HashMap<>();
         result.put("app_key", "6959524271349777951");
         result.put("access_token", "b9e8e8e3-6654-450d-8c1e-9234da785141");
         result.put("timestamp", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         result.put("v", "2");
+        result.put("method", method);
         return result;
     }
 
--- src/main/java/cn/codingstyle/live/commission/commission/model/Order.java
+++ src/main/java/cn/codingstyle/live/commission/commission/model/Order.java
@@ -8,13 +8,33 @@ import java.util.Date;
 @Table(name = "store_order")
 public class Order {
     public static final String SHIPPED = "shipped";
+    //微支付取消
+    public static final String NO_PAYED = "no payed and cancelled";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String orderId;
     private Date createTime;
     private String status;
     private BigDecimal amount;
+
+    public static Order whole(String orderId, int orderStatus, int payAmount, int createTime) {
+        Order order = new Order();
+        order.orderId = orderId;
+        order.status = toStatus(orderStatus);
+        order.amount = BigDecimal.valueOf(payAmount);
+        order.createTime = new Date(createTime * 1000L);
+        return order;
+    }
+
+    //todo: to be refactor
+    private static String toStatus(int orderStatus) {
+        if (orderStatus == 4) {
+            return NO_PAYED;
+        }
+        return "";
+    }
 
 
     public Long getId() {
@@ -47,5 +67,13 @@ public class Order {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
     }
 }
